9bf79a7ac96a27da40467e3eb9b82e8817f86069
len <len@len.ro>
2018-01-12 20:35:13 +0200

    x

--- csv2pgsql.py
+++ csv2pgsql.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import csv
+import sys
 from outputs.pgsql import pgsql
 
 """one time import of csv data into pgsql"""
@@ -8,7 +9,7 @@ from outputs.pgsql import pgsql
 if __name__ == '__main__':
     db = pgsql({"dbname": "piBot", "user": "piBot", "password": "simple", "host": "10.8.0.1", "port": "5432",})
     db.open()
-    with open('data.csv', 'r') as csvfile:
+    with open(sys.argv[1], 'r') as csvfile:
         csvreader = csv.reader(csvfile, dialect='excel')
         for row in csvreader:  
             print('.', end='', flush=True)
